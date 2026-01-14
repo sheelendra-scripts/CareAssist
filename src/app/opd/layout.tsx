c2a08508b243e67f26de03fedb236141a7c59a6e
SHEELENDRA <sheelendra1112@gmail.com>
2026-01-14 17:28:36 +0000

    Try fixing this error: `Runtime Error: useSidebar must be used within a

---
+++ src/app/opd/layout.tsx
@@ -0,0 +1,23 @@
+import { AppSidebar } from '@/components/layout/sidebar';
+import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
+
+export default function OpdLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <div className="md:hidden">
+        {/* On mobile, sidebar is in a sheet and doesn't take space */}
+        <AppSidebar />
+        <main>{children}</main>
+      </div>
+      <div className="hidden md:flex">
+         {/* On desktop, sidebar is part of the layout */}
+        <AppSidebar />
+        <SidebarInset>{children}</SidebarInset>
+      </div>
+    </SidebarProvider>
+  );
+}
